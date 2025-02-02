36bed0b160119be9167a5cc3121240a817761935
Isicheri <tech2dom@gmail.com>
2025-02-02 14:49:34 +0100

    still writing the shell code

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,27 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
+    print!("$ ");
+    io::stdout().flush().unwrap();
+    
  let stdin = io::stdin();
  let mut input = String::new();
  loop {
-    input.clear();
+    stdin.read_line(&mut input).unwrap();
+    let command: Vec<_> = input.trim().split(' ').collect();
+     match command[..] {
+         ["exit", code] => process::exit(code.parse::<i32>().unwrap()),
+         ["echo",..] => print!("{}\n",command[1..].join(" ")),
+         ["type", ..] => match command[1] {
+           "exit" | "echo" | "type" => println!("{} is a shell builtin",command[1]),
+           _=> println!("{}: not found",command[1])
+         },
+         _=> println!("{}: command not found",input.trim())
+     }
+     input.clear();
      print!("$ ");
      io::stdout().flush().unwrap();
-    stdin.read_line(&mut input).unwrap();
-     match input.trim() {
-        input => {
-            if input.starts_with("echo ")  {
-                print!("{}\n", &input[5..])
-            }else if input.starts_with("type ") {
-               let builtin = ["echo","exit"];
-               for i in builtin {
-                   if i == &input[5..] {
-                       print!("{} is a shell builtin\n",&input[5..].trim())
-                   }
-               }
-            }
-        },
-        "exit 0" => break,
-          & _=> {
-            print!("{}: not found\n",input.trim());
-        }
-    }
  }
 }
